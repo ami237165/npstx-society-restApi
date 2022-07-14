00fc3f45741dbe7aef37c2d3931d6f79d1af9d6e
ami237165 <ami237165@gmail.com>
2022-07-14 13:24:41 +0530

    adding cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,33 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,{cors:true});
+  const config = new DocumentBuilder()
+    .setTitle('School REST API Documentation')
+    .setDescription('This document details the rest endpoint that is exposed via http routes for schools')
+    .setVersion('1.0')
+    .build()
+    // const document = SwaggerModule.createDocument(app, config);
+    // SwaggerModule.setup('api', app, document);
+
+    app.use((req, res, next) => {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Accept');
+      next();
+  });
+  app.enableCors({
+    allowedHeaders:"*", 
+    origin: "*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+ // app.useGlobalPipes(new ValidationPipe())
+  
   await app.listen(6020);
 }
 bootstrap();
